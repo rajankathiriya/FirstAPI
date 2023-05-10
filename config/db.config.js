1c677cc94f518b7892dd3712392b413afcef3b1c
Pyramidplac <pyramidd123@gmail.com>
2023-05-10 15:57:10 +0530

    asas

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 //mongo DB create database(collection)
 module.exports = {
-    url: "mongodb+srv://rkofficial:rkofficial@cluster0.qxdxshc.mongodb.net/newFOOD"
+    url: "mongodb+srv://rkofficial:rkofficial@cluster0.cwdcrck.mongodb.net/newFOOD"
 };
