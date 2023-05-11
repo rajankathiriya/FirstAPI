a219ecd7b7d5d97356dd3bdf7cea8cbe8d6aaf33
Pyramidplac <pyramidd123@gmail.com>
2023-05-11 16:11:38 +0530

    change name

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 //mongo DB create database(collection)
 module.exports = {
 
-    url: "mongodb+srv://newfood:newfood@cluster0.erymo37.mongodb.net/newFOOD"
+    url: "mongodb+srv://newfood:newfood@cluster0.erymo37.mongodb.net/food"
 };
